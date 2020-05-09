26e694219058c9f8c0dda064de71b502ad726199
Diego Freniche Brito <diego@freniche.com>
2020-05-09 06:53:18 +0200

    Moved Sample App to same folder level than library

---
+++ SwiftFiglet/SwiftFiglet/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  SwiftFiglet
+//
+//  Created by Diego Freniche Brito on 08/05/2020.
+//  Copyright © 2020 Diego Freniche Brito. All rights reserved.
+//
+
+import Foundation
+import SwiftFigletKit
+
+//print("Hello, World!".printWithFont(font: FKFont()))
+
+let font = SFKFont.from(file: "fonts/starwars.flf")
+
+//let a = [
+//"     ___  $   ",
+//"    /   \\ $   ",
+//"   /  ^  \\$   ",
+//"  /  /_\\  \\$  ",
+//" /  _____  \\$ ",
+//"/__/     \\__\\$",
+//"             $"]
+//font.appendChar(for: "a", char: SFKChar(charLines: a))
+"this".print(withFigletFont: font)
+"is".print(withFigletFont: font)
+"magic".print(withFigletFont: font)
+
+let computerFont = SFKFont.from(file: "fonts/computer.flf")
+
+
+"computer".print(withFigletFont: computerFont)
